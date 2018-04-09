0c93672d4dfe7aad612c195bcb2490126ad6721c
Chris <ckimchris@gmail.com>
2018-04-09 13:35:27 -0700

    second commit

---
+++ images.js
@@ -0,0 +1,20 @@
+var images = [
+  { 
+    file: 'image0001.png',
+    source: 'http://www.example.com/image0001.png'
+  },
+  { 
+    file: 'image0002.png',
+    source: 'http://www.example.com/image0002.png'
+  },
+  { 
+    file: 'image0003.png',
+    source: 'http://www.example.com/image0003.png'
+  },
+  { 
+    file: 'image0004.png',
+    source: 'http://www.example.com/image0004.png'
+  }
+]
+
+module.exports = images;
