7c1a1b1b67dd5bd9bddb217e02fe1b9de5787640
Chris Kim <ckimchris@gmail.com>
2018-04-09 15:51:07 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  consumer_key:         'nNLN3FfOTjcimDgXZFp2cHLGG',
-  consumer_secret:      'fiQULG0AQvVXp8qhPIcFeWeh8ZxfvZ5QMjxD9G27fjkCscSVee',
-  access_token:         '983416744801779712-MzQ21t3duxp9z3SLBZ8J4bPNuaihgP7',
-  access_token_secret:  'B2PiZylZ9SjuLO9tMPHa49Nwi61gMI2rjCTa0mnY3wI07'
-}
-
-module.exports = config;
